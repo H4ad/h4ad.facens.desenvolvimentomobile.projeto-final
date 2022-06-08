4c863df0626749fa63288075ae4739c249e5dcd9
Vinícius Lourenço <vinyygamerlol@gmail.com>
2022-06-08 18:30:02 -0300

    feat(capacitor): added support to capacitor to generate an apk

---
+++ app/android/app/src/main/java/com/facens/highlance/MainActivity.java
@@ -0,0 +1,5 @@
+package com.facens.highlance;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.facens.highlance',
+  appName: 'highlance',
+  webDir: 'dist/app',
+  bundledWebRuntime: false,
+};
+
+export default config;
